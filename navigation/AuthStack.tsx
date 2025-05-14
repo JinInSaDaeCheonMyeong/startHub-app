3a0f3fbf777713263204e6a5d307a831f337d1d8
uranny <164876325+uranny@users.noreply.github.com>
2025-05-14 17:31:06 +0900

    feat : connect SiginScreen to AuthStack

--- navigation/AuthStack.tsx
+++ navigation/AuthStack.tsx
@@ -1,16 +1,19 @@
 import {createStackNavigator} from "@react-navigation/stack";
 import WelcomeScreen from "../screens/WelcomeScreen";
+import SigninScreen from "../screens/SigninScreen";
 
 const Stack = createStackNavigator<AuthStackParamList>()
 
 export type AuthStackParamList = {
     Welcome: undefined;
+    Signin: undefined;
 };
 
 export default function AuthStack() {
     return (
         <Stack.Navigator initialRouteName={"Welcome"}>
             <Stack.Screen name="Welcome" component={WelcomeScreen} />
+            <Stack.Screen name="Signin" component={SigninScreen} />
         </Stack.Navigator>
     )
 };
