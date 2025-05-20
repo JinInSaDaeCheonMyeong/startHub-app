acfcf858ed44bf2b745b923fb12bbe668774613f
uranny <164876325+uranny@users.noreply.github.com>
2025-05-20 23:46:01 +0900

    feat : add SafeAreaView, StatusBsr and SignupInfoScreenProps

--- screens/SignupInfoScreen.tsx
+++ screens/SignupInfoScreen.tsx
@@ -1,8 +1,16 @@
-import { Text, View } from "react-native";
+import { SafeAreaView, StatusBar, Text, View } from "react-native";
+import { Colors } from "../constants/color";
+import BackButton from "../component/BackButton";
+import { StackScreenProps } from "@react-navigation/stack";
+import { AuthStackParamList } from "../navigation/AuthStack";
 
+type SignupInfoScreenProps = StackScreenProps<AuthStackParamList, 'SignupInfo'>
 
-export default function SignupInfoScreen() {
-    return(<View>
+export default function SignupInfoScreen({navigation, route} : SignupInfoScreenProps) {
+    return(
+    <SafeAreaView>
+        <StatusBar barStyle="dark-content" backgroundColor={Colors.white1}/>
         <Text>SignupInfoScreen</Text>
-    </View>)
+    </SafeAreaView>
+    )
 }
