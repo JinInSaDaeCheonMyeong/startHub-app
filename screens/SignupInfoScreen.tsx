6f528ece6a95e3656371fbf2111f345046ccc8fa
uranny <164876325+uranny@users.noreply.github.com>
2025-05-21 09:21:37 +0900

    feat : add progressBar in SignupInfoScreen

--- screens/SignupInfoScreen.tsx
+++ screens/SignupInfoScreen.tsx
@@ -1,16 +1,26 @@
-import { SafeAreaView, StatusBar, Text, View } from "react-native";
+import { SafeAreaView, StatusBar, Text, useWindowDimensions, View } from "react-native";
 import { Colors } from "../constants/color";
 import BackButton from "../component/BackButton";
 import { StackScreenProps } from "@react-navigation/stack";
 import { AuthStackParamList } from "../navigation/AuthStack";
+import * as Progress from 'react-native-progress';
 
 type SignupInfoScreenProps = StackScreenProps<AuthStackParamList, 'SignupInfo'>
 
 export default function SignupInfoScreen({navigation, route} : SignupInfoScreenProps) {
+    const {width} = useWindowDimensions();
     return(
     <SafeAreaView>
         <StatusBar barStyle="dark-content" backgroundColor={Colors.white1}/>
-        <Text>SignupInfoScreen</Text>
+        <BackButton width={18} height={18} color={Colors.black2} onClick={()=>{}} />
+        <Progress.Bar 
+            width={width - 32} 
+            progress={0.3} 
+            color={Colors.primary} 
+            borderColor={Colors.white2}
+            unfilledColor={Colors.white2}
+            borderWidth={0}
+        />
     </SafeAreaView>
     )
 }
