1e3bb272cc6131e1a392b2578b71ce6cf42a254f
uranny <164876325+uranny@users.noreply.github.com>
2025-05-21 23:19:47 +0900

    feat : connect CntactScreen into SignupStack

--- navigation/SignupStack.tsx
+++ navigation/SignupStack.tsx
@@ -2,17 +2,20 @@ import { createStackNavigator } from "@react-navigation/stack"
 import { Text, View } from "react-native";
 import InfoScreen from "../screens/signup/InfoScreen";
 import { Colors } from "../constants/color";
+import ContactScreen from "../screens/signup/ContactScreen";
 
 
 const Stack = createStackNavigator<SignupStackParamList>();
 
 export type SignupStackParamList = {
     Info : undefined
+    Contact : undefined
 }
 
 export default function SignupStack() {
     return(
         <Stack.Navigator
+        initialRouteName="Contact"
         screenOptions={{
             cardStyle : {
                 backgroundColor : Colors.white1,
@@ -20,6 +23,7 @@ export default function SignupStack() {
             headerShown: false
         }}>
             <Stack.Screen name="Info" component={InfoScreen}/>
+            <Stack.Screen name="Contact" component={ContactScreen}/>
         </Stack.Navigator>
     )
 }
--- screens/signup/ContactScreen.tsx
+++ screens/signup/ContactScreen.tsx
@@ -1,7 +1,10 @@
+import { StackScreenProps } from "@react-navigation/stack";
 import { Text, View } from "react-native";
+import { SignupStackParamList } from "../../navigation/SignupStack";
 
+type ContactScreenProps = StackScreenProps<SignupStackParamList, 'Contact'>
 
-export default function ContactScreen() {
+export default function ContactScreen({navigation, route} : ContactScreenProps) {
     return(
     <View>
         <Text>ContactScreen</Text>
