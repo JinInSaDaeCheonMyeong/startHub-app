5a65de2e3de2241aaaf28e71a1434b4a4cc3ab4c
uranny <164876325+uranny@users.noreply.github.com>
2025-05-21 23:06:42 +0900

    feat : create react-native-dropdown-picker and add style

--- screens/signup/InfoScreen.tsx
+++ screens/signup/InfoScreen.tsx
@@ -1,9 +1,19 @@
 import { StyleSheet, Text, TextInput, View } from "react-native";
 import { Colors } from "../../constants/color";
 import CommonButton from "../../component/CommonButton";
+import DropDownPicker from "react-native-dropdown-picker";
+import TopButton from "../../assets/icons/top-arrow-back.svg"
+import BottomButton from "../../assets/icons/bottom-arrow-back.svg"
+import CheckMark from "../../assets/icons/checkmark.svg"
+import { useState } from "react";
 
 
 export default function InfoScreen() {
+
+    const [open, setOpen] = useState(false)
+    const [value, setValue] = useState(null)
+    const dropdownItems = [{label : "남",  value : "남"}, {label : "여",  value : "여"}]
+
     return(
         <View style={styles.mainContainer}>
             <View style={styles.inputContainer}>
@@ -23,6 +33,71 @@ export default function InfoScreen() {
                         <Text style={styles.subText}>회원님의 성별을 선택해주세요!</Text>
                         <Text style={styles.mainText}>{"(선택)당신의 성별이 궁금합니다!"}</Text>
                     </View>
+                    <DropDownPicker
+                        open={open}
+                        value={value}
+                        items={dropdownItems}
+                        setOpen={setOpen}
+                        setValue={setValue}
+                        placeholder="성별"
+                        placeholderStyle={{
+                            color : Colors.gray2,
+                            fontSize : 18,
+                            fontWeight : "medium"
+                        }}
+                        style={{
+                            backgroundColor : Colors.white2,
+                            borderWidth : 0,
+                            borderRadius : 8,
+                            padding : 16
+                        }}
+                        dropDownContainerStyle={{
+                            backgroundColor : Colors.white2,
+                            borderWidth : 0,
+                            borderRadius : 8,
+                        }}
+                        textStyle={{
+                            color : Colors.black2,
+                            fontSize : 18,
+                            fontWeight : "medium",
+                            paddingVertical : 8,
+                            paddingHorizontal : 6,
+                        }}
+                        labelStyle={{
+                            color : Colors.black2,
+                            fontSize : 18,
+                            fontWeight : "medium",
+                            paddingVertical : 8,
+                            paddingHorizontal : 6,
+                        }}
+                        ArrowUpIconComponent={
+                            ({style}) => (
+                            <TopButton 
+                                width={18} 
+                                height={18} 
+                                color={Colors.gray2} 
+                                style={style}
+                            />
+                        )}
+                        ArrowDownIconComponent={
+                            ({style}) => (
+                            <BottomButton
+                                width={18}
+                                height={18}
+                                color={Colors.gray2}
+                                style={style}
+                            />
+                        )}
+                        TickIconComponent={
+                            ({style}) => (
+                            <CheckMark
+                                width={18}
+                                height={18}
+                                color={Colors.gray2}
+                                style={style}
+                            />
+                        )}
+                    />
                 </View>
             </View>
             <CommonButton title="다음으로" onPress={() => {}}/>
