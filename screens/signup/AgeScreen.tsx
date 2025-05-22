6fac85a50e77623f4ec92b50e1d00ec842ec7328
uranny <164876325+uranny@users.noreply.github.com>
2025-05-22 12:41:24 +0900

    feat : connect AgeScreen into SignupStack

--- screens/signup/AgeScreen.tsx
+++ screens/signup/AgeScreen.tsx
@@ -1,7 +1,10 @@
+import { StackScreenProps } from "@react-navigation/stack";
 import { Text, View } from "react-native";
+import { SignupStackParamList } from "../../navigation/SignupStack";
 
+type AgeScreenProps = StackScreenProps<SignupStackParamList, "Age">
 
-export default function AgeScreen() {
+export default function AgeScreen({navigation, route} : AgeScreenProps) {
     return(
         <View>
             <Text>AgeScreen</Text>
