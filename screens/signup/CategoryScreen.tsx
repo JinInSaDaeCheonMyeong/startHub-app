bfc3979946b27271ed402e7b5d95a96cad875470
uranny <164876325+uranny@users.noreply.github.com>
2025-05-27 19:49:59 +0900

    feat : create CategoryScreen

---
+++ screens/signup/CategoryScreen.tsx
@@ -0,0 +1,11 @@
+import { StackScreenProps } from "@react-navigation/stack";
+import { Text, View } from "react-native";
+import { SignupStackParamList } from "../../navigation/SignupStack";
+
+export default function CategoryScreen(){
+    return(
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
