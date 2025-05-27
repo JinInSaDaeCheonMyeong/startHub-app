ca05428ff04070322205078961f106852379ef64
uranny <164876325+uranny@users.noreply.github.com>
2025-05-27 23:36:59 +0900

    feat : add basic constructor in CategorySelectButton.tsx

--- component/auth/CategorySelectButton.tsx
+++ component/auth/CategorySelectButton.tsx
@@ -1,9 +1,26 @@
-import { View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
+import CategoryCheckMark from "../../assets/icons/category/category-check-mark.svg"
+import { Colors } from "../../constants/color";
+import { ReactElement } from "react";
+import { SvgProps } from "react-native-svg";
 
-export default function CategorySelectButton() {
+type CategorySelectButtonProps = {
+    title : string,
+    icons : React.FC<SvgProps>,
+    color : string,
+    onClick : () => void
+}
+
+export default function CategorySelectButton(props : CategorySelectButtonProps) {
     return(
-        <View>
-            
-        </View>
+        <TouchableOpacity onPress={() => {props.onClick()}}>
+            <View>
+                <View>
+                    {<props.icons color={props.color}/>}
+                    <Text>{props.title}</Text>
+                </View>
+                <CategoryCheckMark color={Colors.fintech}/>
+            </View>
+        </TouchableOpacity>
     )
 }
