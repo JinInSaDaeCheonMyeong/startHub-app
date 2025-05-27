3707ecbeb1750db53c652b217f65d624ec41857d
uranny <164876325+uranny@users.noreply.github.com>
2025-05-27 20:05:41 +0900

    chore : create basic constructor

--- screens/signup/CategoryScreen.tsx
+++ screens/signup/CategoryScreen.tsx
@@ -1,13 +1,25 @@
 import { StackScreenProps } from "@react-navigation/stack";
-import { Text, View } from "react-native";
+import { SafeAreaView, ScrollView, Text, View } from "react-native";
 import { SignupStackParamList } from "../../navigation/SignupStack";
+import CommonButton from "../../component/CommonButton";
 
 type CategoryScreenProps = StackScreenProps<SignupStackParamList, "Category">
 
 export default function CategoryScreen({navigation, route} : CategoryScreenProps){
     return(
         <View>
-            <Text></Text>
+            <View>
+                <View>
+                    <Text>드디어 마지막입니다!</Text>
+                    <Text>어떤 주제에 관심 있으신가요?</Text>
+                </View>
+                <ScrollView>
+                </ScrollView>
+            </View>
+            <View>
+                <Text></Text>
+                <CommonButton title="완료" onPress={() => {}}/>
+            </View>
         </View>
     )
 }
