3c2075fa4fccdde7cc3886e3d262ea7c9cd710fb
uranny <164876325+uranny@users.noreply.github.com>
2025-05-28 11:58:11 +0900

    feat : add CategorySelectButton in CategoryScreen

--- screens/signup/CategoryScreen.tsx
+++ screens/signup/CategoryScreen.tsx
@@ -2,6 +2,9 @@ import { StackScreenProps } from "@react-navigation/stack";
 import { SafeAreaView, ScrollView, Text, View } from "react-native";
 import { SignupStackParamList } from "../../navigation/SignupStack";
 import CommonButton from "../../component/CommonButton";
+import CategorySelectButton from "../../component/auth/CategorySelectButton";
+import { Colors } from "../../constants/color";
+import { categorySelectButtons } from "../../constants/categorySelectButtons";
 
 type CategoryScreenProps = StackScreenProps<SignupStackParamList, "Category">
 
@@ -14,6 +17,15 @@ export default function CategoryScreen({navigation, route} : CategoryScreenProps
                     <Text>어떤 주제에 관심 있으신가요?</Text>
                 </View>
                 <ScrollView>
+                    {categorySelectButtons.map((item) => (
+                        <CategorySelectButton
+                            categoryKey={item.category}
+                            title={item.title}
+                            color={item.color}
+                            icons={item.icon}
+                            onClick={(categoryKey) => {}}
+                        />
+                    ))}
                 </ScrollView>
             </View>
             <View>
