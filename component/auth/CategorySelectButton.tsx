abd71c24f39d6897ebb2f747f5244b18f75167c1
uranny <164876325+uranny@users.noreply.github.com>
2025-05-28 11:09:51 +0900

    feat: add selected state and dynamic styling to CategorySelectButton

--- component/auth/CategorySelectButton.tsx
+++ component/auth/CategorySelectButton.tsx
@@ -1,7 +1,7 @@
-import { Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import CategoryCheckMark from "../../assets/icons/category/category-check-mark.svg"
 import { Colors } from "../../constants/color";
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import { SvgProps } from "react-native-svg";
 
 type CategorySelectButtonProps = {
@@ -12,15 +12,55 @@ type CategorySelectButtonProps = {
 }
 
 export default function CategorySelectButton(props : CategorySelectButtonProps) {
+
+    const [isSelected, setIsSelected] = useState(false)
+
     return(
-        <TouchableOpacity onPress={() => {props.onClick()}}>
-            <View>
-                <View>
-                    {<props.icons color={props.color}/>}
-                    <Text>{props.title}</Text>
+        <TouchableOpacity onPress={() => {
+            setIsSelected(!isSelected)
+            props.onClick()
+        }}>
+            <View style={{
+                ...styles.mainContainer,
+                borderColor : props.color,
+                backgroundColor : isSelected ? `${props.color}E6` : Colors.white1
+            }}>
+                <View style={styles.titleContainer}>
+                    {<props.icons
+                    width={32}
+                    height={32} 
+                    color={isSelected ? Colors.white1 : props.color}
+                    />}
+                    <Text style={{
+                        ...styles.titleText, 
+                        color : isSelected ? Colors.white1 : Colors.black2
+                    }}>
+                        {props.title}
+                    </Text>
                 </View>
-                <CategoryCheckMark color={Colors.fintech}/>
+                <CategoryCheckMark color={isSelected ? Colors.white1 : props.color}/>
             </View>
         </TouchableOpacity>
     )
 }
+
+const styles = StyleSheet.create({
+    mainContainer : {
+        width : "100%",
+        paddingVertical : 16,
+        paddingHorizontal : 20,
+        flexDirection : "row",
+        justifyContent : "space-between",
+        borderWidth : 2,
+        borderRadius : 8
+    },
+    titleContainer : {
+        gap : 16,
+        flexDirection : "row",
+        alignItems : "center"
+    },
+    titleText : {
+        fontSize : 18,
+        fontWeight : "semibold"
+    }
+})
