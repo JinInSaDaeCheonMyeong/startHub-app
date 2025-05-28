28e8a78030cdc6d2ba62fa106e91bdca7b7f21dd
uranny <164876325+uranny@users.noreply.github.com>
2025-05-28 17:01:41 +0900

    feat: replace ScrollView with FlatList and add error state handling in CategoryScreen

--- screens/signup/CategoryScreen.tsx
+++ screens/signup/CategoryScreen.tsx
@@ -1,45 +1,90 @@
 import { StackScreenProps } from "@react-navigation/stack";
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
+import { FlatList, SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
 import { SignupStackParamList } from "../../navigation/SignupStack";
 import CommonButton from "../../component/CommonButton";
 import CategorySelectButton from "../../component/auth/CategorySelectButton";
 import { Colors } from "../../constants/color";
 import { categorySelectButtons } from "../../constants/categorySelectButtons";
+import { useState } from "react";
 
 type CategoryScreenProps = StackScreenProps<SignupStackParamList, "Category">
 
 export default function CategoryScreen({navigation, route} : CategoryScreenProps){
+
+    const [errorVisible, setErrorVisible] = useState(false)
+    const [errorText, setErrorText] = useState("안녕")
     let selectCategoryList : string[] = []
+
     return(
-        <View>
-            <View>
-                <View>
-                    <Text>드디어 마지막입니다!</Text>
-                    <Text>어떤 주제에 관심 있으신가요?</Text>
+        <View style={styles.mainContainer}>
+            <View style={styles.inputContainer}>
+                <View style={styles.textBox}>
+                    <Text style={styles.subText}>드디어 마지막입니다!</Text>
+                    <Text style={styles.mainText}>어떤 주제에 관심 있으신가요?</Text>
                 </View>
-                <ScrollView>
-                    {categorySelectButtons.map((item) => (
-                        <CategorySelectButton
-                            key={item.category}
-                            title={item.title}
-                            color={item.color}
-                            icons={item.icon}
-                            onClick={(categoryKey) => {
-                                if (selectCategoryList.includes(categoryKey)) {
-                                    selectCategoryList = selectCategoryList.filter(key => key !== categoryKey);
-                                } else {
-                                    selectCategoryList.push(categoryKey);
-                                }
-                                console.log(selectCategoryList);
-                            }}
-                        />
-                    ))}
-                </ScrollView>
+                <FlatList 
+                data={categorySelectButtons}
+                contentContainerStyle = {{gap : 16}}
+                renderItem={({item}) => (
+                    <CategorySelectButton
+                        key={item.category}
+                        category={item.category}
+                        title={item.title}
+                        color={item.color}
+                        icons={item.icon}
+                        onClick={(categoryKey) => {
+                            if (selectCategoryList.includes(categoryKey)) {
+                                selectCategoryList = selectCategoryList.filter(key => key !== categoryKey);
+                            } else {
+                                selectCategoryList.push(categoryKey);
+                            }
+                            console.log(selectCategoryList);
+                        }}
+                    />
+                )}
+                />
             </View>
-            <View>
-                <Text></Text>
+            <View style={styles.buttonContainer}>
+                {errorVisible && <Text style={styles.errorText}>{errorText}</Text>}
                 <CommonButton title="완료" onPress={() => {}}/>
             </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    mainContainer : {
+        flex : 1,
+        justifyContent : "space-between",
+    },
+    inputContainer : {
+        gap : 24,
+        flex : 1
+    },
+    textBox : {
+        gap : 4
+    },
+    buttonContainer : {
+        paddingTop : 8,
+        gap : 8
+    },
+    categorySelectContainer : {
+        gap : 16
+    },
+    subText : {
+        fontSize : 16,
+        fontWeight : "medium",
+        color : Colors.black2
+    },
+    mainText : {
+        fontSize : 20,
+        fontWeight : "bold",
+        color : Colors.black2
+    },
+    errorText : {
+        textAlign : "center",
+        color : Colors.error,
+        fontSize : 12,
+        fontWeight : "semibold"
+    }
+})
