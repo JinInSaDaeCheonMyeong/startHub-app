271225bd14e312cb80c086775d5afc1c900345c3
uranny <164876325+uranny@users.noreply.github.com>
2025-05-29 20:49:40 +0900

    feat : create InterestType

---
+++ constants/InterestType.ts
@@ -0,0 +1,10 @@
+export const enum InterestType{
+    CONTENT_MEDIA = "콘텐츠/미디어",
+    FINTECH = "핀테크",
+    HEALTHCARE = "헬스케어",
+    BIO = "바이오",
+    EDUCATION_EDUTECH = "교육/에듀테크",
+    IT_SOFTWARE = "IT/소프트웨어",
+    ECOMMERCE = "전자상거래",
+    ETC ="기타"
+}
