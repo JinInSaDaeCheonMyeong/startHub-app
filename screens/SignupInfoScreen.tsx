f9105650234071721b10299968f8a3080e520cd3
uranny <164876325+uranny@users.noreply.github.com>
2025-05-29 19:55:33 +0900

    feat : connect InfoScreen, ContactScreen, AgeScreen, CategoryScreen in SignupInfoScreen

--- screens/SignupInfoScreen.tsx
+++ screens/SignupInfoScreen.tsx
@@ -5,36 +5,73 @@ import { StackScreenProps } from "@react-navigation/stack";
 import { AuthStackParamList } from "../navigation/AuthStack";
 import * as Progress from 'react-native-progress';
 import { useState } from "react";
+import InfoScreen from "./signup/InfoScreen";
+import ContactScreen from "./signup/ContactScreen";
+import AgeScreen from "./signup/AgeScreen";
+import CategoryScreen from "./signup/CategoryScreen";
+import CommonButton from "../component/CommonButton";
 
 type SignupInfoScreenProps = StackScreenProps<AuthStackParamList, 'SignupInfo'>
+
+const InputInfoScreen = [
+    InfoScreen,
+    ContactScreen,
+    AgeScreen,
+    CategoryScreen
+]
 
 export default function SignupInfoScreen({navigation, route} : SignupInfoScreenProps) {
     const {width} = useWindowDimensions();
     const [currentProgress, setCurrentProgress] = useState(1)
-    const maxProgress = 5;
+    const [errorVisible, setErrorVisible] = useState(true)
+    const [errorText, setErrorText] = useState("error text")
+    const CurrentScreen = InputInfoScreen[currentProgress-1]
+    const maxProgress = 4;
+
+    const goBack = () => {
+        if(currentProgress < 2) {
+            navigation.goBack()
+        } else {
+            setCurrentProgress(currentProgress - 1)
+        }
+    }
+
+    const goNext = () => {
+        if(currentProgress >= maxProgress) {
+            navigation.popTo("Start")
+        } else {
+            setCurrentProgress(currentProgress + 1)
+        }
+    }
+
     return(
     <SafeAreaView style={styles.mainContainer}>
         <StatusBar barStyle="dark-content" backgroundColor={Colors.white1}/>
-        <View>
-            <BackButton 
-                width={20} 
-                height={20} 
-                color={Colors.black2} 
-                onClick={()=>{
-                    setCurrentProgress(currentProgress - 1)
-                }}
-            /> 
-        </View>
+        <BackButton 
+            width={20} 
+            height={20} 
+            color={Colors.black2} 
+            onClick={()=>{
+                goBack()
+            }}
+        /> 
         <View style={styles.progressBarContainer}>
             <Text>{`${currentProgress} of ${maxProgress}`}</Text>
             <Progress.Bar 
                 width={width - 32} 
-                progress={currentProgress / maxProgress} 
+                progress={currentProgress / maxProgress}
                 color={Colors.primary} 
                 borderColor={Colors.white2}
                 unfilledColor={Colors.white2}
                 borderWidth={0}
             />
+        </View>
+        <CurrentScreen 
+            onClick={() => {goNext()}}
+        />
+        <View style={styles.buttonContainer}>
+            {errorVisible && <Text style={styles.errorText}>{errorText}</Text>}
+            <CommonButton title={currentProgress == maxProgress ? "다음으로" : "완료"} onPress={() => {goNext()}}/>
         </View>
     </SafeAreaView>
     )
@@ -50,5 +87,15 @@ const styles = StyleSheet.create({
         marginBottom : 36,
         gap : 8,
         alignItems : "flex-end"
-    }
+    },
+    buttonContainer : {
+        paddingTop : 8,
+        gap : 8
+    },
+    errorText : {
+        textAlign : "center",
+        color : Colors.error,
+        fontSize : 12,
+        fontWeight : "semibold"
+    },
 })
