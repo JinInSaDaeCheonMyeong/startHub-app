1cea75220dba3b73a209aab58c286893344e4f41
uranny <164876325+uranny@users.noreply.github.com>
2025-06-03 16:25:48 +0900

    chore : apply style in LocationScreen.tsx

--- screens/signup/LocationScreen.tsx
+++ screens/signup/LocationScreen.tsx
@@ -1,7 +1,8 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import AuthDropDown from "../../component/auth/AuthDropDown";
 import { useState } from "react";
 import { LocationItems } from "../../constants/LocationItems";
+import { Colors } from "../../constants/Color";
 
 
 export default function LocationScreen() {
@@ -10,21 +11,40 @@ export default function LocationScreen() {
     const [value, setValue] = useState("")
 
     return(
-        <View>
-            <View>
-                <View>
-                    <Text>회원님이 사는 지역을 선택해주세요!</Text>
-                    <Text>가까운 주변 공고를 알려드릴게요!</Text>
-                </View>
-                <AuthDropDown
-                    open={open}
-                    value={value}
-                    setOpen={setOpen}
-                    setValue={setValue}
-                    items={LocationItems}
-                    placeholder="지역"
-                />
+        <View style={styles.mainContainer}>
+            <View style={styles.textBox}>
+                <Text style={styles.subText}>회원님이 사는 지역을 선택해주세요!</Text>
+                <Text style={styles.mainText}>가까운 주변 공고를 알려드릴게요!</Text>
             </View>
+            <AuthDropDown
+                open={open}
+                value={value}
+                setOpen={setOpen}
+                setValue={setValue}
+                items={LocationItems}
+                placeholder="지역"
+            />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    mainContainer : {
+        flex : 1,
+        gap : 16,
+        overflow : "hidden"
+    },
+    textBox : {
+        gap : 4
+    },
+    subText : {
+        fontSize : 16,
+        fontWeight : "medium",
+        color : Colors.black2
+    },
+    mainText : {
+        fontSize : 20,
+        fontWeight : "bold",
+        color : Colors.black2
+    }
+})
