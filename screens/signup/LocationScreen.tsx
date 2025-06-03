27c84c58146d48c5795b853483a4622d7d9bbe0c
uranny <164876325+uranny@users.noreply.github.com>
2025-06-03 16:00:36 +0900

    feat : create LocationScreen.tsx

---
+++ screens/signup/LocationScreen.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+
+export default function LocationScreen() {
+    return(
+        <View>
+            <Text>LocationScreen</Text>
+        </View>
+    )
+}
