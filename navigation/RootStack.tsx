bf56ff04bba8e46439f74fa7b66b7b0e923195b7
김도영 <binj1246@gmail.com>
2025-06-04 14:04:47 +0900

    chore : edit RootStack.tsx

--- navigation/RootStack.tsx
+++ navigation/RootStack.tsx
@@ -1,5 +1,8 @@
 import {createStackNavigator} from "@react-navigation/stack";
 import AuthStack from "./AuthStack";
+import {SafeAreaProvider} from "react-native-safe-area-context";
+import { enableScreens } from 'react-native-screens';
+enableScreens();
 
 const Stack = createStackNavigator<RootStackParamList>();
 
@@ -9,8 +12,10 @@ export type RootStackParamList = {
 
 export default function RootStack() {
     return (
-        <Stack.Navigator initialRouteName={"Auth"} screenOptions={{headerShown: false}}>
-            <Stack.Screen name="Auth" component={AuthStack} />
-        </Stack.Navigator>
+        <SafeAreaProvider>
+            <Stack.Navigator initialRouteName={"Auth"} screenOptions={{headerShown: false}}>
+                <Stack.Screen name="Auth" component={AuthStack} />
+            </Stack.Navigator>
+        </SafeAreaProvider>
     )
 };
