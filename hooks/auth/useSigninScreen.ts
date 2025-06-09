6de315ea4cd5e76d9f2418196df035d05c7772f4
uranny <164876325+uranny@users.noreply.github.com>
2025-06-09 13:39:14 +0900

    feat : apply useError in useSigninScreen.ts

--- hooks/auth/useSigninScreen.ts
+++ hooks/auth/useSigninScreen.ts
@@ -1,6 +1,7 @@
 import { StackScreenProps } from "@react-navigation/stack";
 import { AuthStackParamList } from "../../navigation/AuthStack";
 import { useCallback, useState } from "react";
+import { useError } from "../useError";
 
 type SigninScreenProps = StackScreenProps<AuthStackParamList, 'Signin'>;
 
@@ -16,27 +17,47 @@ const INITIAL_FORM_DATA = {
 
 export const useSigninScreen = ({navigation} : SigninScreenProps) => {
     const [formData, setFormData] = useState(INITIAL_FORM_DATA)
-    const [errorVisible, setErrorVisible] = useState(false) 
-    const [errorText, setErrorText] = useState('')
+    const {
+        value : errorValue,
+        handler : errorHandler
+    } = useError()
 
     const updateFormData = useCallback(<K extends keyof FormData>(key : K, value : FormData[K]) => {
         setFormData(prev => ({...prev, [key] : value}))
-        if(errorVisible){
-            setErrorVisible(false)
-            setErrorText("")
+        if(errorValue.errorVisible){
+            errorHandler.hideError()
         }
-    }, [errorVisible])
+    }, [])
 
     const setEmail = useCallback((value : string) => updateFormData("email", value), [updateFormData])
     const setPassword = useCallback((value : string) => updateFormData("password", value), [updateFormData])
 
+    const requestLogin = useCallback(() => {
+        console.log("로그인 요청")
+    }, [])
 
+    const goSignupScreen = useCallback(() => {
+        navigation.navigate("Signup")
+    }, [])
+
+    const goBack = useCallback(() => {
+        navigation.goBack()
+    }, [])
 
     return {
         form : {
             ...formData,
             setEmail,
             setPassword
+        },
+        nav : {
+            requestLogin,
+            goSignupScreen,
+            goBack
+        },
+        ui : {
+            errorVisible : errorValue.errorVisible,
+            errorText : errorValue.errorText
         }
     }
 }
