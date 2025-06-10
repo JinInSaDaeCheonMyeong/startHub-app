084b9934e534e163a0deb62bde3f576160f06f9d
김도영 <binj1246@gmail.com>
2025-06-10 23:25:10 +0900

    feat : apis

--- api/oauth.ts
+++ api/oauth.ts
@@ -1,29 +1,45 @@
 import axios from 'axios';
 import Toast from "react-native-toast-message";
+import {getAccToken, saveAccToken, saveRefToken} from "../util/token";
+import {ShowToast, ToastType} from "../util/ShowToast";
 
 
+const url = process.env.EXPO_PUBLIC_API_URL
 
 export async function getState() {
     try {
-        const response = await axios.get('https://your-server.com/api/auth/google');
-        return response.data;
+        const response = await axios.get(url + '/oauth/state');
+        return response.data.data;
     } catch (error: any) {
-        Toast.show({
-            type: 'error',
-            text1: '문제가 발생하였습니다',
-            text2: error.message,
-        });
+        ShowToast(
+            "실패",
+            error.message,
+            ToastType.ERROR
+        )
     }
 }
 
-export async function getGoogleToken(authCode: string) {
-    try {
-        const response = await axios.post('https://your-server.com/api/auth/google', {
-            code: authCode,
-        });
-        return response.data.accessToken;
-    } catch (error: any) {
-        console.error('토큰 요청 실패:', error?.response?.data || error.message);
-        throw new Error('토큰 요청 실패');
+export async function googleLogin(code: String) {
+    const state = await getState();
+    console.log(state);
+    if (state) {
+        try {
+            const response = await axios.post(url + '/oauth/google',{},{
+                params : {
+                    code : code,
+                    state : state
+                }
+            });
+            console.log(code);
+            await saveAccToken(response.data.data.access);
+            await saveRefToken(response.data.data.refresh);
+            console.log("acc : " + await getAccToken());
+        } catch (error: any) {
+            Toast.show({
+                type: 'error',
+                text1: 'a ++ 문제가 발생하였습니다',
+                text2: error.message,
+            });
+        }
     }
 }
