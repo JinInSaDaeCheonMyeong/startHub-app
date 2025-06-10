e2b7fc61a3eb799eaf8987aac5b039a8377c5393
김도영 <binj1246@gmail.com>
2025-06-10 23:21:09 +0900

    chore : toast set

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import RootStack from "./navigation/RootStack";
 import {NavigationContainer} from "@react-navigation/native";
+import Toast from "react-native-toast-message";
 
 export default function App() {
   return (
       <NavigationContainer>
         <RootStack/>
+        <Toast/>
       </NavigationContainer>
   );
 }
