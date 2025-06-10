8437c6d5965152e465f380e6187942647ee77b08
김도영 <binj1246@gmail.com>
2025-06-10 23:53:40 +0900

    chore : Add error handling

--- api/oauth.ts
+++ api/oauth.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
-import Toast from "react-native-toast-message";
-import {getAccToken, saveAccToken, saveRefToken} from "../util/token";
+import {saveAccToken, saveRefToken} from "../util/token";
 import {ShowToast, ToastType} from "../util/ShowToast";
 
 
@@ -12,34 +11,41 @@ export async function getState() {
         return response.data.data;
     } catch (error: any) {
         ShowToast(
-            "실패",
+            "문제가 발생하였습니다.",
             error.message,
             ToastType.ERROR
         )
+        return;
     }
 }
 
 export async function googleLogin(code: String) {
     const state = await getState();
     console.log(state);
-    if (state) {
-        try {
-            const response = await axios.post(url + '/oauth/google',{},{
-                params : {
-                    code : code,
-                    state : state
-                }
-            });
-            console.log(code);
-            await saveAccToken(response.data.data.access);
-            await saveRefToken(response.data.data.refresh);
-            console.log("acc : " + await getAccToken());
-        } catch (error: any) {
-            Toast.show({
-                type: 'error',
-                text1: 'a ++ 문제가 발생하였습니다',
-                text2: error.message,
-            });
-        }
+    if (!state) {
+        ShowToast(
+            "문제가 발생하였습니다.",
+            "state is undefined",
+            ToastType.ERROR
+        )
+        return;
+    }
+    try {
+        const response = await axios.post(url + '/oauth/google',{},{
+            params : {
+                code : code,
+                state : state
+            }
+        });
+        console.log(code);
+        await saveAccToken(response.data.data.access);
+        await saveRefToken(response.data.data.refresh);
+        return response.data.data.isFirstLogin;
+    } catch (error: any) {
+        ShowToast(
+            '문제가 발생하였습니다.',
+            error.message,
+            ToastType.ERROR,
+        );
     }
 }
