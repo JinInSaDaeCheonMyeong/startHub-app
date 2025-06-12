e21745e27b3a566434fe69ea9d44fd69b9b1f9f9
김도영 <binj1246@gmail.com>
2025-06-12 17:44:19 +0900

    fix : set env

--- api/user/signin.ts
+++ api/user/signin.ts
@@ -2,9 +2,8 @@ import axios from "axios";
 import { SigninRequest, SigninResponse } from "../../type/user/signin.type";
 
 export const signin = async (signinData : SigninRequest) : Promise<SigninResponse> => {
-    console.log(process.env.EXPO_PUBLIC_SERVER_API_BASE_URL)
     const { data } = await axios.post(
-        `${process.env.EXPO_PUBLIC_SERVER_API_BASE_URL}/user/sign-in`,
+        `${process.env.EXPO_PUBLIC_API_URL}/user/sign-in`,
         signinData
     )
     return data;
