00d241ccaa94a72b24ef502f4f044b0e97092370
uranny <164876325+uranny@users.noreply.github.com>
2025-06-19 14:20:13 +0900

    feat : create HeaderBar component with bell and system icons, and logo

---
+++ component/home/HeaderBar.tsx
@@ -0,0 +1,39 @@
+import { Image, StyleSheet, TouchableOpacity, View } from "react-native"
+import { Colors } from "../../constants/Color"
+import BellIcon from "../../assets/icons/header/bell.svg"
+import SystemIcon from "../../assets/icons/header/cogwheel-outline.svg"
+
+type HeaderBarProps = {
+    onClickBellIcon : () => void,
+    onClickSystemIcon : () => void
+}
+
+export const HeaderBar = (props : HeaderBarProps) => {
+    return (
+        <View style={styles.headerContainer}>
+            <Image source={require("../../assets/logos/starthub_title_logo.png")}/>
+            <View style={styles.iconContainer}>
+                <TouchableOpacity onPress={props.onClickBellIcon}>
+                    <BellIcon width={24} height={24} color={Colors.black2}/>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={props.onClickSystemIcon}>
+                    <SystemIcon width={24} height={24} color={Colors.black2}/>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    headerContainer : {
+        width : "100%",
+        backgroundColor : Colors.white1,
+        flexDirection : "row",
+        justifyContent : "space-between",
+        alignItems : "center",
+    },
+    iconContainer : {
+        flexDirection : "row",
+        gap : 16
+    }
+})
