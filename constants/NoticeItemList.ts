3f033f372e98afe60edaed464dd350fbb495a0a1
uranny <164876325+uranny@users.noreply.github.com>
2025-07-08 19:02:40 +0900

    chore : rename dummy constants/NoticeItemList.st

--- constants/NoticeItemList.ts
+++ constants/NoticeItemList.ts
@@ -1,29 +1,29 @@
 import { NoticeItemType } from "../type/notice/notice.type";
 import { NoticeCategory } from "./NoticeCategory";
 
-export const NoticeListItemList : NoticeItemType[] = [
+export const NoticeItemList : NoticeItemType[] = [
     {
         id : 1,
-        noticeCategory : NoticeCategory.BUSINESS,
+        category : NoticeCategory.BUSINESS,
         title : "string1string1string1string1string1string1string1string1string1string1",
         startTime : new Date(),
         endTime : new Date(),
-        hashTagList : ["안녕", "안녕", "안녕","안녕", "안녕", "안녕","안녕", "안녕", "안녕","안녕", "안녕", "안녕"]
+        hashTags : ["안녕", "안녕", "안녕","안녕", "안녕", "안녕","안녕", "안녕", "안녕","안녕", "안녕", "안녕"]
     },
     {
         id : 2,
-        noticeCategory : NoticeCategory.BUSINESS,
+        category : NoticeCategory.BUSINESS,
         title : "string2",
         startTime : new Date(),
         endTime : new Date(),
-        hashTagList : ["안녕", "안녕", "안녕"]
+        hashTags : ["안녕", "안녕", "안녕"]
     },
     {
         id : 3,
-        noticeCategory : NoticeCategory.BUSINESS,
+        category : NoticeCategory.BUSINESS,
         title : "string3",
         startTime : new Date(),
         endTime : new Date(),
-        hashTagList : ["안녕", "안녕", "안녕"]
+        hashTags : ["안녕", "안녕", "안녕"]
     }
 ]
