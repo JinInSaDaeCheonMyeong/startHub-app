df9f764db72144baec1de8dc68bb4c866bef105a
uranny <164876325+uranny@users.noreply.github.com>
2025-07-10 09:37:49 +0900

    feat : create constants/dummy/ChatDummy.ts and type/chat/chatRoom.ts

---
+++ constants/dummy/ChatDummy.ts
@@ -0,0 +1,46 @@
+import { ChatRoomType } from "../../type/chat/chatRoom.type";
+
+export const dummyChatRommList : ChatRoomType[] = [
+    {
+        id : 1,
+        userName : "name dummy1",
+        lastMsg : "msg dummy1",
+        date : new Date(2025, 7, 9, 8, 58, 0),
+        img : "https://www.gstatic.com/lamda/images/gemini_thumbnail_v2_55a4e3be7b83404a620e5.jpg"
+    },
+    {
+        id : 2,
+        userName : "name dummy2",
+        lastMsg : "msg dummy2",
+        date : new Date(2025, 7, 8, 8, 58, 0),
+        img : "https://www.gstatic.com/lamda/images/gemini_thumbnail_v2_55a4e3be7b83404a620e5.jpg"
+    },
+    {
+        id : 3,
+        userName : "name dummy3",
+        lastMsg : "msg dummy3",
+        date : new Date(2025, 7, 7, 8, 58, 0),
+        img : "https://www.gstatic.com/lamda/images/gemini_thumbnail_v2_55a4e3be7b83404a620e5.jpg"
+    },
+    {
+        id : 4,
+        userName : "name dummy4",
+        lastMsg : "msg dummy4",
+        date : new Date(2025, 7, 6, 8, 58, 0),
+        img : "https://www.gstatic.com/lamda/images/gemini_thumbnail_v2_55a4e3be7b83404a620e5.jpg"
+    },
+    {
+        id : 5,
+        userName : "name dummy5",
+        lastMsg : "msg dummy5",
+        date : new Date(2025, 7, 5, 8, 58, 0),
+        img : "https://www.gstatic.com/lamda/images/gemini_thumbnail_v2_55a4e3be7b83404a620e5.jpg"
+    },
+    {
+        id : 6,
+        userName : "name dummy6",
+        lastMsg : "msg dummy6",
+        date : new Date(2025, 5, 30, 9, 19, 0),
+        img : "https://www.gstatic.com/lamda/images/gemini_thumbnail_v2_55a4e3be7b83404a620e5.jpg"
+    }
+]
---
+++ type/chat/chatRoom.type.ts
@@ -0,0 +1,8 @@
+
+export interface ChatRoomType{
+    id : number,
+    userName : string,
+    lastMsg : string,
+    date : Date,
+    img : string
+}
