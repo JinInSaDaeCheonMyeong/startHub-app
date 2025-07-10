e23ba3f71bb9b6c6d953fe95a7005b7527ec2251
uranny <164876325+uranny@users.noreply.github.com>
2025-07-10 10:26:30 +0900

    feat : create util/DateFromat.ts and fix style ChatScreen.ts

--- constants/dummy/ChatDummy.ts
+++ constants/dummy/ChatDummy.ts
@@ -5,42 +5,70 @@ export const dummyChatRommList : ChatRoomType[] = [
         id : 1,
         userName : "name dummy1",
         lastMsg : "msg dummy1",
-        date : new Date(2025, 7, 9, 8, 58, 0),
+        date : new Date(2025, 6, 9, 8, 58, 0),
         img : "https://www.gstatic.com/lamda/images/gemini_thumbnail_v2_55a4e3be7b83404a620e5.jpg"
     },
     {
         id : 2,
         userName : "name dummy2",
         lastMsg : "msg dummy2",
-        date : new Date(2025, 7, 8, 8, 58, 0),
+        date : new Date(2025, 6, 8, 8, 58, 0),
         img : "https://www.gstatic.com/lamda/images/gemini_thumbnail_v2_55a4e3be7b83404a620e5.jpg"
     },
     {
         id : 3,
         userName : "name dummy3",
         lastMsg : "msg dummy3",
-        date : new Date(2025, 7, 7, 8, 58, 0),
+        date : new Date(2025, 6, 7, 8, 58, 0),
         img : "https://www.gstatic.com/lamda/images/gemini_thumbnail_v2_55a4e3be7b83404a620e5.jpg"
     },
     {
         id : 4,
         userName : "name dummy4",
         lastMsg : "msg dummy4",
-        date : new Date(2025, 7, 6, 8, 58, 0),
+        date : new Date(2025, 6, 6, 8, 58, 0),
         img : "https://www.gstatic.com/lamda/images/gemini_thumbnail_v2_55a4e3be7b83404a620e5.jpg"
     },
     {
         id : 5,
         userName : "name dummy5",
         lastMsg : "msg dummy5",
-        date : new Date(2025, 7, 5, 8, 58, 0),
+        date : new Date(2025, 6, 5, 8, 58, 0),
         img : "https://www.gstatic.com/lamda/images/gemini_thumbnail_v2_55a4e3be7b83404a620e5.jpg"
     },
     {
         id : 6,
         userName : "name dummy6",
         lastMsg : "msg dummy6",
-        date : new Date(2025, 5, 30, 9, 19, 0),
+        date : new Date(2025, 6, 10, 9, 19, 0),
+        img : "https://www.gstatic.com/lamda/images/gemini_thumbnail_v2_55a4e3be7b83404a620e5.jpg"
+    },
+    {
+        id : 7,
+        userName : "name dummy7",
+        lastMsg : "msg dummy7",
+        date : new Date(2025, 6, 10, 9, 20, 0),
+        img : "https://www.gstatic.com/lamda/images/gemini_thumbnail_v2_55a4e3be7b83404a620e5.jpg"
+    },
+    {
+        id : 8,
+        userName : "name dummy8",
+        lastMsg : "msg dummy8",
+        date : new Date(2025, 6, 10, 9, 19, 0),
+        img : "https://www.gstatic.com/lamda/images/gemini_thumbnail_v2_55a4e3be7b83404a620e5.jpg"
+    },
+    {
+        id : 9,
+        userName : "name dummy9",
+        lastMsg : "msg dummy9",
+        date : new Date(2025, 6, 10, 9, 20, 0),
+        img : "https://www.gstatic.com/lamda/images/gemini_thumbnail_v2_55a4e3be7b83404a620e5.jpg"
+    },
+    {
+        id : 10,
+        userName : "name dummy10",
+        lastMsg : "msg dummy10",
+        date : new Date(2025, 6, 10, 9, 10, 0),
         img : "https://www.gstatic.com/lamda/images/gemini_thumbnail_v2_55a4e3be7b83404a620e5.jpg"
     }
 ]
--- screens/Home/ChatScreen.tsx
+++ screens/Home/ChatScreen.tsx
@@ -3,86 +3,111 @@ import SearchBar from "../../component/home/SearchBar";
 import { Shadow } from "react-native-shadow-2";
 import { Colors } from "../../constants/Color";
 import { Fonts } from "../../constants/Fonts";
-import { Menu, PaperProvider } from "react-native-paper";
+import {PaperProvider } from "react-native-paper";
 import { ChatMenuButton } from "../../component/home/ChatMenuButton";
+import { dummyChatRommList } from "../../constants/dummy/ChatDummy";
+import { getDateDifference } from "../../util/dateFormat";
+import { ChatRoomType } from "../../type/chat/chatRoom.type";
 
 export default function ChatScreen() {
+
+    const chatRoomListSort = (list : ChatRoomType[]) => 
+    [...list].sort((a, b) => b.date.getTime() - a.date.getTime())
+
     return (
         <PaperProvider>
         <SafeAreaView style={styles.mainContainer}>
-            <SearchBar onPress={(text : string) => {console.log(text)}}/>
-            <Shadow
-                distance={4} 
-                offset={[0, 4]} 
-                startColor="rgba(185, 185, 185, 0.2)"
-                style={{
-                    width : "100%"
-                }}
-            >
-                <View style={styles.chatContainer}>
-                    <Text style={styles.mainText}>내 채팅</Text>
-                    <FlatList
-                        data={["1", "2", "3", "4", "", "3", "3", "3", "3", "3", "3", "3", "3", "3", "3", "3"]}
-                        contentContainerStyle={{gap : 16}}
-                        renderItem={({item}) => (
-                            <TouchableOpacity>
-                            <View style={styles.chatRoomContainer}>
-                                <Image 
-                                    style={{
-                                        width : 48, 
-                                        height : 48,
-                                        borderRadius : 8
-                                    }}
-                                    source={{uri : "https://www.gstatic.com/lamda/images/gemini_thumbnail_v2_55a4e3be7b83404a620e5.jpg"}}
-                                />
-                                <View style={styles.chatInfoContainer}>
-                                    <Text 
-                                        style={styles.userText}
-                                        numberOfLines={1}
-                                        ellipsizeMode="tail"
+            <View style={styles.searchContainer}>
+                <SearchBar onPress={(text : string) => {console.log(text)}}/>
+            </View>
+            <View style={styles.chatWrapper}>
+                <Shadow
+                    distance={4} 
+                    offset={[0, 4]} 
+                    startColor="rgba(185, 185, 185, 0.2)"
+                    style={styles.shadowStyle}
+                >
+                    <View style={styles.chatContainer}>
+                        <Text style={styles.mainText}>내 채팅</Text>
+                            <FlatList
+                                showsVerticalScrollIndicator={false}
+                                data={chatRoomListSort(dummyChatRommList)}
+                                contentContainerStyle={{gap : 16}}
+                                renderItem={({item}) => (
+                                    <TouchableOpacity onPress={() => {console.log(item.id)}}>
+                                    <View 
+                                        style={styles.chatRoomContainer}
+                                        key={item.id}
                                     >
-                                        Dummy User Name
-                                    </Text>
-                                    <Text style={styles.msgText}>
-                                        Dummy Message
-                                    </Text>
-                                </View>
-                                <View style={styles.menuContainer}>
-                                    <Text style={styles.dateText}>      
-                                        Dummy Date                          
-                                    </Text>
-                                    <ChatMenuButton
-                                        onDelete={() => {console.log("삭제하기")}}
-                                    />
-                                </View>
-                            </View>
-                            </TouchableOpacity>
-                        )}
-                    />
-                </View>
-            </Shadow>
+                                        <Image 
+                                            style={{
+                                                width : 48, 
+                                                height : 48,
+                                                borderRadius : 8
+                                            }}
+                                            source={{uri : item.img}}
+                                        />
+                                        <View style={styles.chatInfoContainer}>
+                                            <Text 
+                                                style={styles.userText}
+                                                numberOfLines={1}
+                                                ellipsizeMode="tail"
+                                            >
+                                                {item.userName}
+                                            </Text>
+                                            <Text style={styles.msgText}>
+                                                {item.lastMsg}
+                                            </Text>
+                                        </View>
+                                        <View style={styles.menuContainer}>
+                                            <Text style={styles.dateText}>      
+                                                {getDateDifference(item.date)}                       
+                                            </Text>
+                                            <ChatMenuButton
+                                                onDelete={() => {console.log("삭제하기")}}
+                                            />
+                                        </View>
+                                    </View>
+                                    </TouchableOpacity>
+                                )}
+                            />
+                        </View>
+                </Shadow>
+            </View>
         </SafeAreaView>
         </PaperProvider>
     )
 
 }
+
 const styles = StyleSheet.create({
     mainContainer : {
         flex: 1,
         flexDirection: 'column',
-        margin : 16,
-        gap : 16
+    },
+    searchContainer: {
+        paddingHorizontal: 16,
+        paddingTop: 16,
+    },
+    chatWrapper: {
+        flex: 1,
+        paddingHorizontal: 16,
+        paddingVertical: 16,
+    },
+    shadowStyle: {
+        width: "100%",
     },
     chatContainer : {
-        flex : 1,
+        flexDirection: 'column',
         borderRadius : 8,
         backgroundColor : Colors.white1,
         gap : 16,
-        padding : 16
+        padding : 16,
+        maxHeight : "100%"
     },
     chatRoomContainer : {
         flexDirection : 'row',
-        flex : 1,
+        width : "100%",
         justifyContent : 'space-between',
         gap : 8
     },
