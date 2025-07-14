794dff93a92b654117041e350bcfbdb6ce29f998
uranny <164876325+uranny@users.noreply.github.com>
2025-07-14 20:54:07 +0900

    fix : fix GetUserResponse

--- type/user/user.type.ts
+++ type/user/user.type.ts
@@ -2,11 +2,12 @@ import { Response } from "../util/response.type";
 
 export interface GetUserResponse extends Response {
     data : {
-        id : string
+        id : number
         email : string
         username : string
         birth : string
         gender : string
-        profileImage : string
+        profileImage : string,
+        introduction : string
     }
 }
