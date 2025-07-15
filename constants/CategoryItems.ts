84378c8341d37c7279cc3863362da481877a5c6f
김도영 <binj1246@gmail.com>
2025-07-15 13:25:01 +0900

    chore : edit CategoryItems.ts

--- constants/CategoryItems.ts
+++ constants/CategoryItems.ts
@@ -5,9 +5,6 @@ export const CategoryItems = [
     { label: "교육", value: "교육" },
     { label: "글로벌", value: "글로벌" },
     { label: "인력", value: "인력" },
-    { label: "융자", value: "융자" },
     { label: "행사", value: "행사" },
-    { label: "창업교육", value: "창업교육" },
-    { label: "글로벌", value: "해외진출" },
     { label: "자금", value: "정책자금" },
 ];
